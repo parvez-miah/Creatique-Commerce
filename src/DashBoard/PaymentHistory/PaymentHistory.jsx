80f45408f8bd360d60e011a1aa722ca9d3dbb821
unknown <fbhelpsbd@gmail.com>
2023-08-21 11:50:48 +0600

    added some

--- src/DashBoard/PaymentHistory/PaymentHistory.jsx
+++ src/DashBoard/PaymentHistory/PaymentHistory.jsx
@@ -3,12 +3,15 @@ import {
     IconButton,
     Alert,
     AlertIcon,
+    Spinner,
 } from '@chakra-ui/react';
 import { CopyIcon } from '@chakra-ui/icons';
 import { Helmet } from 'react-helmet-async';
+import { MdPayments } from 'react-icons/md';
 
 const PaymentHistory = () => {
     const [copySuccess, setCopySuccess] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [paymentHistory, setPaymentHistory] = useState([]);
 
     const copyToClipboard = (text) => {
@@ -27,49 +30,66 @@ const PaymentHistory = () => {
     useEffect(() => {
         fetch('https://creatique-commerce-server.vercel.app/payments')
             .then((res) => res.json())
-            .then((data) => setPaymentHistory(data));
+            .then((data) => {
+                setPaymentHistory(data);
+                setLoading(false);
+            });
     }, []);
 
     return (
-        <div style={{marginTop:'90px'}}>
-            {copySuccess && (
-                <Alert status='success'>
-                    <AlertIcon />
-                    TransactionId copied!
-                </Alert>
-            )}
-            <Helmet>
-                <title> Payment History | Creatique Commerce </title>
-                <link rel="canonical" href="https://www.tacobell.com/" />
-            </Helmet>
-            <div className="overflow-x-auto">
-                <table className="table table-zebra w-full">
-                    {/* head */}
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Transaction ID</th>
-                            <th>Copy ID</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {paymentHistory.map((payments, index) => (
-                            <tr key={index}>
-                                <td>{index + 1}</td>
-                                <td>{payments.email}</td>
-                                <td>{payments.transactionId}</td>
-                                <td>
-                                    <IconButton
-                                        aria-label='Copy'
-                                        icon={<CopyIcon />}
-                                        onClick={() => copyToClipboard(payments.transactionId)}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+        <div>
+            <h2 className="text-red-500 text-3xl font-semibold p-5 flex items-center mt-8">
+                <MdPayments className="text-black" />
+                <span className="ml-2">Payments</span>
+            </h2>
+            <div style={{ marginTop: '90px' }}>
+                {copySuccess && (
+                    <Alert status='success'>
+                        <AlertIcon />
+                        TransactionId copied!
+                    </Alert>
+                )}
+                <Helmet>
+                    <title> Payment History | Creatique Commerce </title>
+                    <link rel="canonical" href="https://www.tacobell.com/" />
+                </Helmet>
+                {loading ? (
+                    <Spinner size="xl" color="blue.500" />
+                ) : (
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full">
+                            <thead>
+                                <tr>
+                                    <th className="py-2">
+                                        <label>
+                                            <span>#</span>
+                                        </label>
+                                    </th>
+                                    <th className="py-2">Email</th>
+                                    <th className="py-2">Transaction ID</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {paymentHistory.map((payments, index) => (
+                                    <tr key={payments._id}>
+                                        <td className="py-2">{index + 1}</td>
+                                        <td className="py-2">
+                                            <span className="badge badge-ghost badge-sm">{payments.email}</span>
+                                        </td>
+                                        <td className="py-2">
+                                            {payments.transactionId.slice(0, 5)}...{payments.transactionId.slice(-2)}
+                                            <IconButton
+                                                aria-label='Copy'
+                                                icon={<CopyIcon />}
+                                                onClick={() => copyToClipboard(payments.transactionId)}
+                                            />
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                )}
             </div>
         </div>
     );
